2c0306cb06524184ca0adb0073a5633d8ae80dea
Nimo <nimo.jser@gmail.com>
2018-04-06 13:53:42 +0800

    onface/module auto-generated commit

--- 0.2.0/doc/basic.demo.js
+++ 0.2.0/doc/basic.demo.js
@@ -76,31 +76,37 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
 var dc = __webpack_require__("./lib/index.js");(typeof global === 'undefined' ? 'undefined' : _typeof(global)) ? global.dcDebug = true : window.dcDebug = true;
 // https://nodejs.org/api/globals.html
 function query(attrs, data) {
+    /**/
     dc.group('query.js');
     dc.$input('query', attrs, data);
+    /**/
     function getQueryArray(attrs) {
         return attrs.split(',');
     }
-
+    /**/
     dc.group('getQueryArray');
     dc.$input('getQueryArray', attrs);
+    /**/
     var queryArray = getQueryArray(attrs);
+    /**/
     dc.$output('getQueryArray', queryArray);
     dc.groupEnd();
-
     dc.groupCollapsed('queryArray.forEach(setKey)');
+    /**/
     var output = {};
     queryArray.forEach(function setKey(key) {
+        /**/
         dc.log('output["' + key + '"] = ', '"' + data[key] + '"');
+        /**/
         output[key] = data[key];
     });
+    /**/
     dc.groupEnd();
-
     dc.$output('query', output);
     dc.$show();
+    /**/
     return output;
 }
-
 query('a,b,c', {
     a: 'nimo',
     b: 'nico',
